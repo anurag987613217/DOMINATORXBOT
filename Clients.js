0799bd6241da8910c12d573f91a53de5da28d4dc
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-20 08:41:11 -0700

    Update Clients.js

--- Clients.js
+++ Clients.js
@@ -2,12 +2,12 @@ from import=Legendhacker_iin
          javascript_mirror_mod=x
               import=legendhacker()else:
               lse=on
-           ultronbot_loop_session.on
+           DOMINATORbot_loop_session.on
                                    try()legendhacker_poweroffff==if 
-                                      UltronBot="ALIVE_MSG"
+                                      DOMINATORBot="ALIVE_MSG"
                                       CMD-HELPER"help"
                                        ALIVE_PIC=''
-                                       ULTRONBOT_STRING=''
+                                       DOMINATORBOT_STRING=''
 else:(@LegendHacker_IIN ("FROM") (:THE SCORE-LEGEND:)
                            print:("MADRCHODD BAAP HU BAAP HI RHUGA")
                                   ("KIYA KHA THA OKAD")
