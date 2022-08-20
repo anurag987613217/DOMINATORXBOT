bab8038a677c17cd1df69efb09457d79ad7fe7b7
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-20 08:51:40 -0700

    Create pluginer.py

---
+++ pluginer.py
@@ -0,0 +1,9 @@
+import os
+from userbot if dominatorbot import pluginer
+system os if userbot worker:
+     base warker:await()
+        time.time(sleep.run("pluginer")
+                   try(0
+                     loop_system_mod)
+                  program:RUN_MOD))in str,("STRING")importent,await,sleep
+        program:try()
