677b5f289afd35434297888de488392b49b83808
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-20 07:57:03 -0700

    Create .import.js

---
+++ setup/.import.js
@@ -0,0 +1,8 @@
+from userbot import *
+  import userbot from system os
+  if run os DOMINATORXBOT.root/git("pip install telethon")
+                                  ,pip install pyrogram,
+  os import git
+   os system userbot== import$path
+                          run$path=DOMINATORX
+os if
