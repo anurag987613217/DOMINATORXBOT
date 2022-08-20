d991e760c759534a868db941953881abd9d9af3f
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-20 08:36:49 -0700

    Update and rename ultron_string.py to dominator_string.py

--- dominator_string.py
+++ dominator_string.py
@@ -6,26 +6,26 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 
-print("•••   ULTRONBOT  SESSION  GENERATOR   •••")
-print("\nHello!! Welcome to UltronBot Session Generator\n")
+print("••• DOMINATORBOT  SESSION  GENERATOR   •••")
+print("\nHello!! Welcome to DOMINATORBot Session Generator\n")
 okvai = input("Enter 69 to continue: ")
 if okvai == "69":
-    print("Choose the string session type: \n1. ultronBot \n2. Music Bot")
+    print("Choose the string session type: \n1. DOMINATORnBot \n2. Music Bot")
     library = input("\nYour Choice: ")
     if library == "1":
         print("\nTelethon Session For HellBot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with TelegramClient(StringSession(), APP_ID, API_HASH) as ultronbot:
-            print("\nYour UltronBot Session Is sent in your Telegram Saved Messages.")
-            ultronbot.send_message("me", f"#ULTRONBOT #ULTRONBOT_SESSION \n\n`{ultronbot.session.save()}`")
+            print("\nYour DOMINATORBot Session Is sent in your Telegram Saved Messages.")
+            ultronbot.send_message("me", f"#DOMINATORBOT #DOMINATORBOT_SESSION \n\n`{ultronbot.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as ultronbot:
             print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
-            ultronbot.send_message("me", f"#ULTRONBOT_MUSIC #ULTRONBOT_SESSION\n\n`{ultronbot.export_session_string()}`")
+            ultronbot.send_message("me", f"#DOMINATORNBOT_MUSIC #DOMINATORBOT_SESSION\n\n`{ultronbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
