1181f27247b875f917c1c28acacf2337004e5704
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-20 08:46:56 -0700

    Update dominator_string.py

--- dominator_string.py
+++ dominator_string.py
@@ -13,19 +13,19 @@ if okvai == "69":
     print("Choose the string session type: \n1. DOMINATORnBot \n2. Music Bot")
     library = input("\nYour Choice: ")
     if library == "1":
-        print("\nTelethon Session For HellBot")
+        print("\nTelethon Session For DominatorBot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with TelegramClient(StringSession(), APP_ID, API_HASH) as ultronbot:
+        with TelegramClient(StringSession(), APP_ID, API_HASH) as dominatorbot:
             print("\nYour DOMINATORBot Session Is sent in your Telegram Saved Messages.")
-            ultronbot.send_message("me", f"#DOMINATORBOT #DOMINATORBOT_SESSION \n\n`{ultronbot.session.save()}`")
+            dominatorbot.send_message("me", f"#DOMINATORBOT #DOMINATORBOT_SESSION \n\n`{dominatorbot.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
-        with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as ultronbot:
+        with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as dominatorbot:
             print("\nYour DOMINATORBot Session Is sent in your Telegram Saved Messages.")
-            ultronbot.send_message("me", f"#DOMINATORNBOT_MUSIC #DOMINATORBOT_SESSION\n\n`{ultronbot.export_session_string()}`")
+            dominatorbot.send_message("me", f"#DOMINATORNBOT_MUSIC #DOMINATORBOT_SESSION\n\n`{dominatorbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
