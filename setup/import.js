164c0752dc379e15aa171dfedaf61991f60f73e7
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-08-26 09:13:27 -0700

    Delete import.js

--- setup/import.js
+++
@@ -1,7 +0,0 @@
-from dominatorbot import*
-from userbot import python ,java ,linux
-import userbot file.
-java==(int("dominatorBot")
-       cloner import+program.utils
-       cloner javascript.utils.py():
-       loop_ayvsdd:()
